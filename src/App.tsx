e63951ca4f6ee9e12f94aa2dfc282d93d4bd3e47
Vinicius Reis <vinireis010305@gmail.com>
2024-03-12 15:58:54 -0300

    config query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
+import { queryClient } from './lib/query'
 import { router } from './routes'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/defaults'
@@ -9,7 +11,9 @@ function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
---
+++ src/env.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  MODE: z.enum(['development', 'production', 'test']),
+  VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
+})
+
+export const env = envSchema.parse(import.meta.env)
