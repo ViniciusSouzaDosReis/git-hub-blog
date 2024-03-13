3fbce1dd42e4913b8d68d8544c27f5e0e2c1fd5c
Vinicius Reis <vinireis010305@gmail.com>
2024-03-13 12:08:13 -0300

    Refactor API response and update PostCard component

--- src/pages/Blog/components/PostCard/index.tsx
+++ src/pages/Blog/components/PostCard/index.tsx
@@ -1,24 +1,19 @@
 import { PostCardContainer } from './styles'
 
-export function PostCard() {
+interface PostCardProps {
+  title: string
+  updateDate: string
+  bio: string
+}
+
+export function PostCard({ bio, title, updateDate }: PostCardProps) {
   return (
     <PostCardContainer to="/post/123">
       <div>
-        <h2>JavaScript data types and data structures</h2>
-        <span>Há 1 dia</span>
+        <h2>{title}</h2>
+        <span>{updateDate}</span>
       </div>
-      <p>
-        Programming languages all have built-in data structures, but these often
-        differ from one language to another. This article attempts to list the
-        built-in data structures available in JavaScript and what properties
-        they have. These can be used to build other data structures. Wherever
-        possible, comparisons with other languages are drawn. Dynamic typing
-        JavaScript is a loosely typed and dynamic language. Variables in
-        JavaScript are not directly associated with any particular value type,
-        and any variable can be assigned (and re-assigned) values of all types:
-        let foo = 42; // foo is now a number foo =; // foo is now a string foo =
-        true; // foo is now a boolean
-      </p>
+      <p>{bio}</p>
     </PostCardContainer>
   )
 }
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,23 +1,65 @@
+import { useQuery } from '@tanstack/react-query'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { useState } from 'react'
+
+import { getRepoIssues } from '@/api/get-repo-issues'
+
 import { PostCard } from './components/PostCard'
 import { ProfileCard } from './components/ProfileCard'
 import { ListPostCard, SearchFormContainer } from './styles'
 
 export function Blog() {
+  const [search, setSearch] = useState('')
+
+  const { data: postsData } = useQuery({
+    queryKey: ['posts', search],
+    queryFn: () => getRepoIssues({ searchText: search }),
+    staleTime: Infinity,
+  })
+
+  function handleSearchIssue(data: React.ChangeEvent<HTMLInputElement>) {
+    const value = data.target.value
+    setSearch(value)
+  }
+
   return (
     <>
       <ProfileCard />
       <SearchFormContainer>
         <div>
           <strong>Publicações</strong>
-          <span>6 publicações</span>
+          <span>{postsData?.total_count} publicações</span>
         </div>
-        <input type="text" placeholder="Buscar conteúdo" />
+        <input
+          type="text"
+          placeholder="Buscar conteúdo"
+          value={search}
+          onChange={(event) => handleSearchIssue(event)}
+        />
       </SearchFormContainer>
-      <ListPostCard>
-        <PostCard />
-        <PostCard />
-        <PostCard />
-      </ListPostCard>
+      {postsData ? (
+        <>
+          <ListPostCard>
+            {postsData.items.map((item) => {
+              const date = formatDistanceToNow(item.updated_at, {
+                locale: ptBR,
+              })
+
+              return (
+                <PostCard
+                  key={item.id}
+                  bio={item.body}
+                  title={item.title}
+                  updateDate={date}
+                />
+              )
+            })}
+          </ListPostCard>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   )
 }
