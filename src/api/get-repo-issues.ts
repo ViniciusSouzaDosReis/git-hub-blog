e3efec9d173f51a2ccddc47ae9798c4161db3961
Vinicius Reis <vinireis010305@gmail.com>
2024-03-13 12:08:08 -0300

    Add getRepoIssues function to fetch repository issues

---
+++ src/api/get-repo-issues.ts
@@ -0,0 +1,23 @@
+import { api } from '@/lib/axios'
+
+interface GetRepoIssuesParams {
+  searchText: string
+}
+
+interface GetRepoIssuesResponse {
+  total_count: number
+  items: {
+    id: number
+    body: string
+    title: string
+    updated_at: string
+  }[]
+}
+
+export async function getRepoIssues({ searchText }: GetRepoIssuesParams) {
+  const response = await api.get<GetRepoIssuesResponse>(
+    `/search/issues?q=${searchText} repo:microsoft/TypeScript`,
+  )
+
+  return response.data
+}
